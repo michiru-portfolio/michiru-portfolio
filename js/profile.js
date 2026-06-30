7699168710d302bf3be5cd0ce14775660224fe37
大地みちる <michiru.o.0408@gmail.com>
2026-06-30 22:55:43 +0900

    Profileのボタンとアニメーション追加

--- js/profile.js
+++ js/profile.js
@@ -13,3 +13,32 @@ window.addEventListener("scroll", () => {
     }
 
 });
+
+const enterBtn = document.querySelector(".next-btn");
+
+if (enterBtn) {
+
+    enterBtn.addEventListener("click", function(event){
+        
+        // すぐページ移動しないようにする
+        event.preventDefault();
+
+        // リンク先を取得
+        const url = this.href;
+
+        //　メッセージ表示
+        document.getElementById("transition-message").classList.add("show");
+
+        // bodyにfade-outを付ける
+        document.body.classList.add("fade-out");
+
+        // 0.8秒後にページ移動
+        setTimeout(() => {
+
+            window.location.href = url;
+
+        },1500);
+
+    });
+
+}
