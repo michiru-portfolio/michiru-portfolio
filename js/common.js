f83bb26ffb64dc103c3271b84fd5a1c81b16b293
大地みちる <michiru.o.0408@gmail.com>
2026-07-01 14:58:11 +0900

    ヘッダーとフッターの追加

--- js/common.js
+++ js/common.js
@@ -70,3 +70,27 @@ section.classList.add("fade-up");
 observer.observe(section);
 
 });
+
+// ===============================
+// Header
+// ===============================
+
+fetch("components/header.html")
+.then(response => response.text())
+.then(data => {
+
+    document.getElementById("header").innerHTML = data;
+
+});
+
+// ===============================
+// Footer
+// ===============================
+
+fetch("components/footer.html")
+.then(response => response.text())
+.then(data => {
+
+    document.getElementById("footer").innerHTML = data;
+
+});
