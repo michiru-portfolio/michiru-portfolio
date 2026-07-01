c5535a2d4bfeeaea35999a88ef2b704f07fe503f
大地みちる <michiru.o.0408@gmail.com>
2026-07-01 12:15:00 +0900

    Recordsに細かい演出追加、portfolio-site追加

--- js/records.js
+++ js/records.js
@@ -1,8 +1,8 @@
-const cards = document.querySelectorAll(".card");
+const books = document.querySelectorAll(".book-card");
 
-const observer = new IntersectionObserver((entries) => {
+const observer = new IntersectionObserver((entries)=>{
 
-    entries.forEach(entry => {
+    entries.forEach(entry=>{
 
         if(entry.isIntersecting){
 
@@ -14,8 +14,68 @@ const observer = new IntersectionObserver((entries) => {
 
 });
 
-cards.forEach(card => {
+books.forEach(book=>{
 
-    observer.observe(card);
+    observer.observe(book);
 
 });
+
+// =========================
+// 本を開く演出
+// =========================
+
+const openButtons = document.querySelectorAll(".open-book");
+
+openButtons.forEach(button => {
+
+    button.addEventListener("click", function(event){
+
+        event.preventDefault();
+
+        const url = this.href;
+
+        document
+            .getElementById("transition-message")
+            .classList.add("show");
+
+        document.body.classList.add("fade-out");
+
+        setTimeout(() => {
+
+            window.location.href = url;
+
+        },1800);
+
+    });
+
+});
+
+// =========================
+// Galleryへ移動
+// =========================
+
+const nextBtn = document.querySelector(".next-btn");
+
+if(nextBtn){
+
+    nextBtn.addEventListener("click", function(event){
+
+        event.preventDefault();
+
+        const url = this.href;
+
+        document
+            .getElementById("transition-message")
+            .classList.add("show");
+
+        document.body.classList.add("fade-out");
+
+        setTimeout(()=>{
+
+            window.location.href = url;
+
+        },1800);
+
+    });
+
+}
